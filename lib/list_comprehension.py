3d76fa5995128f3a9e084fe8df2b896c74c32a02
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-09-14 18:51:04 -0400

    yessir

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    num_list = [x for x in num_list if x % 2 == 0]
+    return num_list
     pass
 
 def make_exclamation(sentence_list):
+    sentence_list = [x + "!" for x in sentence_list]
+    return sentence_list
+        
     pass
